e65655af251e2f42934c5e715dc2338225c8b567
YankiiOz <yankii-blimm@mail.ru>
2021-03-02 16:20:48 +0300

    added autoprefixer and clean-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,25 @@
   var gulp = require('gulp');
   var less = require('gulp-less');
   var path = require('path');
+  var uglify = require('gulp-uglify');
+  var cleanCSS = require('gulp-clean-css');
+  const autoprefixer = require('gulp-autoprefixer');
  
+exports.prefix = () => (
+    gulp.src('./css/style.css')
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(gulp.dest('./css/prefix.css'))
+);
+
+ 
+gulp.task('minify-css', () => {
+  return gulp.src('./css/style.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./css/min.css'));
+});
+
 gulp.task('less', function () {
   return gulp.src('./less/*.less')
     .pipe(less({
